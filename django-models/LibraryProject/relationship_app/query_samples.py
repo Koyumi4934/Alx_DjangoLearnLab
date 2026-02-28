32bd8b45a04cee666036f1fa243ba1d803ff8053
WILLY KIPTOO NGENOH <KIPTOOWILLY1@GMAIL.COM>
2026-02-28 11:20:10 +0300

    Fix query_samples.py to include required queries

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,21 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Query all books by a specific author
-author = Author.objects.get(name="George Orwell")
-books_by_author = Book.objects.filter(author=author)
-print(books_by_author)
 
 # List all books in a library
-library = Library.objects.get(name="City Library")
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
-print(books_in_library)
+print("Books in library:", books_in_library)
+
+
+# Query all books by a specific author
+author_name = "J.K. Rowling"
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
+print("Books by author:", books_by_author)
+
 
 # Retrieve the librarian for a library
+library = Library.objects.get(name=library_name)
 librarian = Librarian.objects.get(library=library)
-print(librarian)
+print("Librarian:", librarian)
